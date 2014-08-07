cc142f5158a07dbb3dc0718f93736b2dca1d9361
Owen Versteeg <me@owenversteeg.com>
2014-08-07 02:51:57 -0400

    Clean up docs/naming/CSS (closes #34)

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir compiled -p
 #The compiled folder holds all the separate compiled CSS files.
 
-subtypes=(general buttons grid headings icons inputs navbar tables)
+subtypes=(general buttons grid headings icons forms navbar tables)
 #You can change the subtypes used by changing this.
 
 rm -f compiledcss.js
