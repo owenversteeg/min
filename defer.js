c223f9d083afb6d79aad45bde483dbf512179443
Owen Versteeg <me@owenversteeg.com>
2014-09-20 20:39:17 -0400

    update analytics to include enhanced link stuff

--- defer.js
+++ defer.js
@@ -13,6 +13,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-41177412-1', 'auto');
+ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
 
 //Quantcast
